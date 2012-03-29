e636cc4df4512e083378e1ac820d526261f230eb
Thomas Johnson <xenofixus@gmail.com>
2012-03-29 03:29:49 -0400

    Moved helper classes to separate package

---
+++ src/com/github/igp/IGHelpers/BlockSetter.java
@@ -0,0 +1,23 @@
+package com.github.igp.IGHelpers;
+
+import org.bukkit.block.Block;
+import org.bukkit.inventory.ItemStack;
+
+public class BlockSetter implements Runnable
+{
+	private final Block block;
+	private final ItemStack stack;
+
+	public BlockSetter(final Block block, final ItemStack stack)
+	{
+		this.block = block;
+		this.stack = stack;
+	}
+
+	@Override
+	public void run()
+	{
+		block.setType(stack.getType());
+		block.setData(stack.getData().getData());
+	}
+}
