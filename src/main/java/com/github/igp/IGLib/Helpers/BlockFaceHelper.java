1b7be9b8a818a460dcf8366539ab4d0f06943bc0
Thomas Johnson <xenofixus@gmail.com>
2012-10-07 15:47:44 -0400

    Changed directory structure, updated pom.xml

---
+++ src/main/java/com/github/igp/IGLib/Helpers/BlockFaceHelper.java
@@ -0,0 +1,44 @@
+package com.github.igp.IGLib.Helpers;
+
+import org.bukkit.block.BlockFace;
+
+public class BlockFaceHelper
+{
+	private static final BlockFace[] adjacentFaces =
+	{
+			BlockFace.UP,
+			BlockFace.DOWN,
+			BlockFace.NORTH,
+			BlockFace.EAST,
+			BlockFace.SOUTH,
+			BlockFace.WEST
+	};
+
+	public static BlockFace[] getAdjacentFaces()
+	{
+		return adjacentFaces;
+	}
+
+	public static BlockFace getOppositeFace(final BlockFace face)
+	{
+		if (face.equals(BlockFace.UP))
+			return BlockFace.DOWN;
+
+		if (face.equals(BlockFace.DOWN))
+			return BlockFace.UP;
+
+		if (face.equals(BlockFace.NORTH))
+			return BlockFace.SOUTH;
+
+		if (face.equals(BlockFace.SOUTH))
+			return BlockFace.NORTH;
+
+		if (face.equals(BlockFace.EAST))
+			return BlockFace.WEST;
+
+		if (face.equals(BlockFace.WEST))
+			return BlockFace.EAST;
+
+		return null;
+	}
+}
